3ec6c3429a60165a991d41a3bb92872b15b51bff
Can Yilmaz <can@yilmaz.app>
2023-08-27 13:59:16 +0100

    proper layout

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -4,6 +4,7 @@
   import '@fortawesome/fontawesome-free/css/all.css'
   import '$lib/i18n'
   import { messages } from '$lib/app.messages.svelte'
+  import '../styles.css'
 
   export async function preload() {
     // awaits for the loading of the 'en-US' and 'en' dictionaries
