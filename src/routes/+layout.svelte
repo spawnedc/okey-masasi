5be793cde137c68fccd4882ee874a152027e36e9
Can Yilmaz <can@yilmaz.app>
2023-08-27 02:41:02 +0100

    set title from translations

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,14 +1,18 @@
 <script>
   import 'bulma/css/bulma.css'
+  import { _, waitLocale } from 'svelte-i18n'
   import '@fortawesome/fontawesome-free/css/all.css'
   import '$lib/i18n'
-
-  import { waitLocale } from 'svelte-i18n'
+  import { messages } from '$lib/app.messages.svelte'
 
   export async function preload() {
     // awaits for the loading of the 'en-US' and 'en' dictionaries
     return waitLocale()
   }
 </script>
+
+<svelte:head>
+  <title>{$_(messages.appTitle.id)}</title>
+</svelte:head>
 
 <slot />
